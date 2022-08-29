8747ad6a7980e32fcea1522f63bdc5d20f050404
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-08-29 15:21:26 +0700

    finish section 03

--- Section 03 Volumes/data-volumes-01-starting-setup/data-volumes-01-starting-setup/server.js
+++ Section 03 Volumes/data-volumes-01-starting-setup/data-volumes-01-starting-setup/server.js
@@ -31,6 +31,8 @@ app.post("/create", async (req, res) => {
   const tempFilePath = path.join(__dirname, "temp", adjTitle + ".txt");
   const finalFilePath = path.join(__dirname, "feedback", adjTitle + ".txt");
 
+  console.log("test");
+
   await fs.writeFile(tempFilePath, content);
   exists(finalFilePath, async (exists) => {
     if (exists) {
@@ -43,4 +45,4 @@ app.post("/create", async (req, res) => {
   });
 });
 
-app.listen(80);
+app.listen(process.env.PORT);
